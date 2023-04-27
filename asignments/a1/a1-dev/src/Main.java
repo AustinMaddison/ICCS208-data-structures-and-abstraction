8419688aa7489ac9bf01bdf067bfff56e7181862
Austin Maddison <Austin.madison10152@gmail.com>
2023-04-27 23:19:54 +0700

    Task 4 - Fibonacci, completed

--- asignments/a1/a1-dev/src/Main.java
+++ asignments/a1/a1-dev/src/Main.java
@@ -2,14 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
         /* Task 2 - Diamond */
-        Diamond.printDiamond(10);
+//        Diamond.printDiamond(10);
         
         /* Task 3 - Roman to Number */
-        System.out.println(Roman.romanToInt("IV"));
-        System.out.println(Roman.romanToInt("I"));
-        System.out.println(Roman.romanToInt("VII"));
-        System.out.println(Roman.romanToInt("MCMLIV"));
-        System.out.println(Roman.romanToInt("MCMXC"));
+//        System.out.println(Roman.romanToInt("IV"));
+//        System.out.println(Roman.romanToInt("I"));
+//        System.out.println(Roman.romanToInt("VII"));
+//        System.out.println(Roman.romanToInt("MCMLIV"));
+//        System.out.println(Roman.romanToInt("MCMXC"));
+
+        /* Task 4 - NDigit Fibonacci Number */
+        System.out.println(Fib.firstNDigit(1));
+        System.out.println(Fib.firstNDigit(100));
+
+
     }
 
 }
