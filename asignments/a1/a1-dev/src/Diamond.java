d1243d91b36115a6271161c790959838a5929977
Austin Maddison <Austin.madison10152@gmail.com>
2023-04-27 20:17:22 +0700

    a1-Diamond

--- asignments/a1/a1-dev/src/Diamond.java
+++ asignments/a1/a1-dev/src/Diamond.java
@@ -1,18 +1,46 @@
+/**
+ * @author Austin Maadison
+ * @date 27/04/2023
+ */
 public class Diamond {
     public static void printDiamond(int k){
         int width = k*2 + 1;
         int height = k*2 -1;
 
-        for(int row = 0; row < height; row++){
+        char[][] map = new char[k][width];
 
-            for(int column = 0; column < width; column++){
-                System.out.print("#");
-                System.out.print("*");
+        // render half+middle of diamond to map
+        for(int row = 0; row < k; row++){
+            int hashes = k*2 -(row*2);
+            int stars = width - hashes;
+            int column = 0;
+            for(int left_hashes = hashes/2; left_hashes > 0; left_hashes--){
+                map[row][column] = '#';
+                column++;
+            }
+            for(; stars > 0; stars--){
+                map[row][column] = '*';
+                column++;
+            }
+            for(int right_hashes = hashes/2; right_hashes > 0; right_hashes--){
+                map[row][column] = '#';
+                column++;
+            }
+        }
+
+        // print map to console including reflection for bottom
+        for(int row = 0; row < k; row++){
+            for(int col = 0; col < width; col++){
+                System.out.print(map[row][col]);
             }
             System.out.printf("%n");
         }
-
-
+        for(int row = k-2; row >= 0; row--) {
+            for(int col = 0; col < width; col++){
+                System.out.print(map[row][col]);
+            }
+            System.out.printf("%n");
+        }
 
 
 
